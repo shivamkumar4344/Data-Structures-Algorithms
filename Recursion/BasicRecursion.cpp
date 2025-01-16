3dc4b547c18ee852cf972b9e5f57554fd59d5187
Shivam Kumar <ydvshivam10@gmail.com>
2025-01-16 20:18:52 +0530

    adding basic recursion

---
+++ Recursion/BasicRecursion.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int factorial(int n){
+    if(n==0 || n==1){
+        return 1;
+    }
+    int ans = n * factorial(n-1);
+    return ans;
+}
+
+void printCounting(int n){
+    if(n==1){
+        cout<<1<<" ";
+        return;
+    }
+    printCounting(n-1);
+    cout<<n<<" ";
+    
+}
+
+int pow2(int n){
+    if(n == 0)
+    {//base condition
+        return 1;
+    }
+
+    return 2 * pow2(n-1);
+}
+
+int main()
+{
+    int n = 7;
+    cout<<factorial(n)<<endl;
+    printCounting(n);
+    cout<<endl;
+    cout<<pow2(n)<<endl;
+
+    return 0;
+
+}
