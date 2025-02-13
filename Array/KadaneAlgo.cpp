6f379011b557ad54b365998fb895821a004bbcdd
Shivam Kumar <ydvshivam10@gmail.com>
2025-02-13 09:45:16 +0530

    max sub array sum

---
+++ Array/KadaneAlgo.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> arr = {-2,1,3,-3};
+    int currSum = 0;
+    int maxSum = arr[0];
+    for(int i=0;i<arr.size();i++)
+    {
+        currSum += arr[i];
+        if(currSum > maxSum){
+            maxSum = max(currSum,maxSum);
+        }
+        if(currSum < 0){
+            currSum = 0;
+        }
+    }
+    cout<<maxSum<<endl;
+}
