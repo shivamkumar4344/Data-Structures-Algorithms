47e6d03ed3768fdc3ec37b260a389e6f3b889585
Shivam Kumar <ydvshivam10@gmail.com>
2025-02-25 23:59:47 +0530

    adding updated file

--- Trees/BSTFromPreOrder.cpp
+++ Trees/BSTFromPreOrder.cpp
@@ -47,12 +47,55 @@ void createBST(Node* &root)
     }
 }
 
+Node* solve(vector<int>& preOrder,int mini,int maxi,int &i){
+
+    if(i >= preOrder.size()) return NULL;
+
+    if(preOrder[i] < mini or preOrder[i] > maxi) return NULL;
+
+    Node* root = new Node(preOrder[i++]);
+    root->left = solve(preOrder,mini,root->data,i);
+    root->right = solve(preOrder,root->data,maxi,i);
+
+    return root;
+}
+
+void levelOrderTraversal(Node* root)
+{
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty())
+    {
+        Node* front = q.front();
+        q.pop();
+
+        if(front == NULL)
+        {
+            cout<<endl;
+            if(!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else{
+            cout<<front->data<<" ";
+            if(front->left != NULL) q.push(front->left);
+            if(front->right != NULL) q.push(front->right); 
+        }
+    }
+}
 
 
 int main()
 {
     Node* root = NULL;
-    createBST(root);
+    // createBST(root);
 
+    vector<int> preOrder = {20,10,5,15,13,35,30,42};
+    int i = 0;
+    root = solve(preOrder,INT_MIN,INT_MAX,i);
+    levelOrderTraversal(root);
     return 0;
 }
