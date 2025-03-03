90709899b8e62169b64329e62b3e6447b90e9bde
Shivam Kumar <ydvshivam10@gmail.com>
2025-03-03 19:12:01 +0530

    updating boundary conditions

--- Heap/MaxHeapify.cpp
+++ Heap/MaxHeapify.cpp
@@ -1,38 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-//0-based indexing
-void max_heapify(int arr[],int n,int index)
+// 0-based indexing
+void max_heapify(int arr[], int n, int index)
 {
-    int left = 2*index+1;
-    int right = 2*index+2;
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
     int largest = index;
 
-    if(n >= left && arr[largest] < arr[left]) largest = left;
-    if(n >= right && arr[largest] < arr[right]) largest = right;
+    if (n > left && arr[largest] < arr[left])
+        largest = left;
+    if (n > right && arr[largest] < arr[right])
+        largest = right;
 
-    if(index != largest)
+    if (index != largest)
     {
-        swap(arr[index],arr[largest]);
+        swap(arr[index], arr[largest]);
         // index = largest;
-        max_heapify(arr,n,largest);
+        max_heapify(arr, n, largest);
     }
 }
 
-void buildHeap(int arr[],int n)
+void buildHeap(int arr[], int n)
 {
-    for(int i=n/2-1;i>=0;i--)
+    for (int i = n / 2 - 1; i >= 0; i--)
     {
-        max_heapify(arr,n,i);
+        max_heapify(arr, n, i);
     }
 }
 
 int main()
 {
-    int arr[6] = {5,10,15,20,25,12};
-    buildHeap(arr,6);
-    for(int i=1;i<=6;i++)
+    int arr[6] = {5, 10, 15, 20, 25, 12};
+    buildHeap(arr, 6);
+    for (int i = 0; i < 6; i++)
     {
-        cout<<arr[i]<<" ";
-    }cout<<endl;
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
--- Heap/MinHeapify.cpp
+++ Heap/MinHeapify.cpp
@@ -1,40 +1,42 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-//0-based indexing
-void min_heapify(int arr[],int n,int index)
+// 0-based indexing
+void min_heapify(int arr[], int n, int index)
 {
-    int left = 2*index+1;
-    int right = 2*index+2;
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
     int smallest = index;
 
-    if(n >= left && arr[smallest] > arr[left]) smallest = left;
-    
-    if(n >= right && arr[smallest] > arr[right]) smallest = right;
+    if (left < n && arr[smallest] > arr[left])
+        smallest = left;
 
-    if(index != smallest)
+    if (right < n && arr[smallest] > arr[right])
+        smallest = right;
+
+    if (index != smallest)
     {
-        swap(arr[smallest],arr[index]);
-        min_heapify(arr,n,smallest);
+        swap(arr[smallest], arr[index]);
+        min_heapify(arr, n, smallest);
     }
 }
 
-void buildHeap(int arr[],int n)
+void buildHeap(int arr[], int n)
 {
-    for(int i= n/2-1 ;i>=0 ;i--)
+    for (int i = n / 2 - 1; i >= 0; i--)
     {
-        min_heapify(arr,n,i);
+        min_heapify(arr, n, i);
     }
 }
-
 
 int main()
 {
-    int arr[6] = {25,20,15,5,10,12};
-    buildHeap(arr,6);
-    for(int i=0;i<6;i++)
+    int arr[6] = {25, 20, 15, 5, 10, 12};
+    buildHeap(arr, 6);
+    for (int i = 0; i < 6; i++)
     {
-        cout<<arr[i]<<" ";
-    }cout<<endl;
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
