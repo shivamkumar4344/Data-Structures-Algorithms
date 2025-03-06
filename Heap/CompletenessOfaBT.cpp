2d6899fc14e57adbb9401719dfc0a4eb5b34c4af
Shivam Kumar <ydvshivam10@gmail.com>
2025-03-06 14:53:21 +0530

    adding a leetcode question

--- Heap/CompletenessOfaBT.cpp
+++ Heap/CompletenessOfaBT.cpp
@@ -1,7 +1,72 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+
+Node* createTree()
+{
+    int data;
+    cout<<"Enter the data : ";
+    cin>>data;
+
+    if(data == -1){
+        return NULL;
+    }
+
+    Node* root = new Node(data);
+    root->left = createTree();
+    root->right = createTree();
+
+    return root;
+}
+
+bool checkCompleteness(Node* root)
+{
+    queue<Node*> q;
+    q.push(root);
+    bool flag = false;
+    while(!q.empty())
+    {
+        Node* front = q.front();
+        q.pop();
+
+        if(front == nullptr) flag = true;
+
+        else{
+            if(flag) return false;
+
+            q.push(front->left);
+            q.push(front->right);
+        }
+
+    }
+    return true;
+}
+
 int main()
 {
+    Node* root = createTree();;
+    
+
+    if(checkCompleteness(root))
+    {
+        cout<<"Tree is Completeness of a Binary Tree"<<endl;
+    }
+    else{
+        cout<<"Tree is not Completeness of a Binary Tree"<<endl;
+    }
+
     return 0;
 }
