f72600a15475929daaeec936c68141c16131735e
Shivam Kumar <ydvshivam10@gmail.com>
2025-03-16 18:33:45 +0530

    tech interview question

---
+++ Array/test.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+// 1 0 0 0 1 0 0 1 k = 2
+int countSubarr(vector<int>& arr,int k)
+{
+    unordered_map<int,int> mp;
+    mp[0] = 1;
+    int count1 = 0;
+    int res = 0;
+    for(int i:arr)
+    {
+        count1+=i;
+        if(mp.find(count1-k) != mp.end() )
+        {
+            res+=mp[count1-k];
+        }
+        mp[count1]++;
+    }
+    return res;
+
+}
+
+int main()
+{
+    vector<int> arr = {1, 0, 0, 0, 1, 0, 0 ,1};
+    int k = 2;
+    int ans = countSubarr(arr,k);
+    cout<<ans<<endl;
+}
