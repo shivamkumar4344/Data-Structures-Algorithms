eda372ae92a8fd7c193cb5d4ce861c66fd266704
Shivam Kumar <ydvshivam10@gmail.com>
2025-03-21 01:32:11 +0530

    adding tabulation in code

--- DynammicProgramming/0_1KnapSack.cpp
+++ DynammicProgramming/0_1KnapSack.cpp
@@ -35,6 +35,31 @@ int KanpSackUsingMemo(int capacity, vector<int> &profit, vector<int> &weight, in
     return dp[capacity][index];
 }
 
+int knapSackUsingTabu(int capacity,vector<int>& profit,vector<int>& weight,int n)
+{
+    vector<vector<int>> dp(capacity+1,vector<int>(n+1,-1));//creation of dp array
+    //initialisation of dp array
+    for(int i=0;i<=capacity;i++)
+    {
+        dp[i][n] = 0;
+    }
+    for(int i=0;i<=capacity;i++)
+    {
+        for(int j = n-1;j>=0;j--)
+        {
+            int inc = 0;
+            if(weight[j] <= i)
+            {
+                inc = profit[j] + dp[i-weight[j]][j+1];
+            }
+            int exc = 0 + dp[i][j+1];
+            dp[i][j] = max(inc,exc);
+        }
+
+    }
+    return dp[capacity][0];
+}
+
 int main()
 {
     int capacity = 50;
@@ -44,7 +69,8 @@ int main()
     int index = 0;
     vector<vector<int>> dp(capacity + 1, vector<int>(n + 1, -1));
     // int ans = KanpSackUsingRec(capacity,profit,weight,n,index);
-    int ans2 = KanpSackUsingMemo(capacity, profit, weight, n, index, dp);
-    cout << "Max Profit : " << ans2 << endl;
+    // int ans2 = KanpSackUsingMemo(capacity, profit, weight, n, index, dp);
+    int ans3 = knapSackUsingTabu(capacity,profit,weight,n);
+    cout << "Max Profit : " << ans3 << endl;
     return 0;
 }
