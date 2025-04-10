ea361ead3213c60da52e5d95b04f0af99700314a
Shivam Kumar <ydvshivam10@gmail.com>
2025-04-10 22:33:35 +0530

    adding leetcode question/code

---
+++ Char Arrays and String/IndexOfFirstOccurenceOfString.cpp
@@ -0,0 +1,25 @@
+/*LC - 28. Find the Index of the First Occurrence in a String
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int indexFirst(string& big,string& small)
+{
+    int n = big.length();
+    int m = small.length();
+
+    for(int i=0;i<=n-m;i++)
+    {
+        if(big.substr(i,m) == small)
+            return i;
+    }
+    return -1;
+}
+
+int main()
+{
+    string big = "butsaad";
+    string small = "sad";
+    cout<<"Answer is : "<<indexFirst(big,small)<<endl;
+    return 0;
+}
